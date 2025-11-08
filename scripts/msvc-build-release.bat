c1d1aabe05c7a17ec9b6739b23148e366a06b003
Mao JunJie <jasonmao0802@gmail.com>
2025-11-08 11:01:11 -0500

    remove duplicate linker flag options

--- scripts/msvc-build-release.bat
+++ scripts/msvc-build-release.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set LINKER_FLAGS=/link -incremental:no -opt:ref raylib.lib kernel32.lib user32.lib shell32.lib winmm.lib gdi32.lib opengl32.lib -incremental:no -opt:ref
+set LINKER_FLAGS=/link -incremental:no -opt:ref raylib.lib kernel32.lib user32.lib shell32.lib winmm.lib gdi32.lib opengl32.lib
 set COMMON_FLAGS=/DGAME_INTERNAL=0 -Ox -O2 -Os -Ot -GL -Oi -FC
 set EXE_NAME=game.exe
 
