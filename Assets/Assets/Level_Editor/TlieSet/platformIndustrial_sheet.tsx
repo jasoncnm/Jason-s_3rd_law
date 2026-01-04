65f02c22d3ff18ef18b59620e5afb9e1de378bcc
Heartless2000 <h26lyu@gmail.com>
2026-01-04 16:55:32 -0500

    update
    update

---
+++ Assets/Assets/Level_Editor/TlieSet/platformIndustrial_sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="platformIndustrial_sheet" tilewidth="110" tileheight="110" tilecount="25" columns="5">
+ <image source="../../kenney_platformer-pack-industrial/Spritesheet/platformIndustrial_sheet.png" width="620" height="620"/>
+</tileset>
