ea05f5175453f7e74fcea3bf6ccd849b17edb60a
Mao JunJie <jasonmao0802@gmail.com>
2026-01-13 22:05:42 -0500

    small code change

--- src/entity.cpp
+++ src/entity.cpp
@@ -663,7 +663,8 @@ inline bool8 IsProjectable(IVec2 tilePos, IVec2 pushDir)
         {
             if ((ent->type == ENTITY_TYPE_ELECTRIC_DOOR) &&
                 (ent->cableType != CABLE_TYPE_DOOR || !SameSide(ent, ent->tilePos, dirs[i]))
-                || (ent->type == ENTITY_TYPE_GLASS && ent->broken))
+                || (ent->type == ENTITY_TYPE_GLASS && ent->broken)
+                || (IsSlime(ent) && ent->attach && dirs[i] != pushDir))
             {
                 continue;
             }
