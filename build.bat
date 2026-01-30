692d8976bc3e81b3896028661c8891a073197992
junjie mao <jasonmao0802@gmail.com>
2026-01-30 18:09:35 -0500

    update release build script to build game and raylib together, udpate github action to build release build as artifacts

--- build.bat
+++ build.bat
@@ -4,10 +4,9 @@ if "%1" == "clean" (del /Q bin\*)
 
 call scripts\msvc_upgrade_cmd_64.bat
  
-call scripts\msvc-build-raylib.bat
-
 if "%1" == "release" (
 	call scripts\msvc-build-release.bat
 ) else  (
+	call scripts\msvc-build-raylib.bat
 	call scripts\msvc-build-debug.bat
 )
--- scripts/msvc-build-release.bat
+++ scripts/msvc-build-release.bat
@@ -1,13 +1,18 @@
 @echo off
 
-set LINKER_FLAGS=/link -incremental:no -opt:ref raylib.lib kernel32.lib user32.lib shell32.lib winmm.lib gdi32.lib opengl32.lib
+set LINKER_FLAGS=/link -incremental:no -opt:ref kernel32.lib user32.lib shell32.lib winmm.lib gdi32.lib opengl32.lib
 set COMMON_FLAGS=/std:c++17 /DGAME_INTERNAL=0 -Ox -O2 -Os -Ot -GL -Oi -FC /MT
 set EXE_NAME=game.exe
-set INCLUDES=-I..\src\vendor\raylib\ -I..\src\vendor\raygui\src\
+set DEFINES=/DPLATFORM_DESKTOP
 
 cd bin
 
-cl %INCLUDES% ..\src\main.cpp /Fe:%EXE_NAME% %COMMON_FLAGS% %LINKER_FLAGS% %WARNINGS% %DEFINES%
+set INCLUDES=-I..\src\vendor\raylib\ -I..\src\vendor\raygui\src\ -I..\src\vendor\raylib\src\external\glfw\include
+
+set LIBPATH=..\src\vendor\raylib\src
+set SOURCE_FILES=..\src\main.cpp %LIBPATH%\rcore.c %LIBPATH%\rmodels.c %LIBPATH%\raudio.c %LIBPATH%\rglfw.c %LIBPATH%\rshapes.c %LIBPATH%\rtext.c %LIBPATH%\rtextures.c
+
+cl %DEFINES% %SOURCE_FILES% %INCLUDES%  /Fe:%EXE_NAME% %COMMON_FLAGS% %LINKER_FLAGS% %WARNINGS% 
 
 cd ..
 
