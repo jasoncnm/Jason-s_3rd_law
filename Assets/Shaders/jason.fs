d2de78f826ee95a74a2e7826e72aaf51f4e37380
Heartless2000 <h26lyu@gmail.com>
2026-02-15 15:39:10 -0500

    update

--- Assets/Shaders/jason.fs
+++ Assets/Shaders/jason.fs
@@ -1,13 +1,14 @@
 #version 330
 
-
 // Input vertex attributes (from vertex shader)
 in vec2 fragTexCoord;
 in vec4 fragColor;
 
+
 // Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
+
 
 // Output fragment color
 out vec4 finalColor;
@@ -15,48 +16,14 @@ out vec4 finalColor;
 // NOTE: Add your custom variables here
 const float samples = 2;          // Pixels per axis; higher = bigger glow, worse performance
 const float quality = 6;          // Defines size factor: Lower = smaller glow, better quality
-float stitchingSize = 5.0;
 float gamma = 0.8;
 float numColors = 10.0;
 
-
-uniform int invert = 0;
+// Custom uniforms
 uniform vec2 u_frameSize;
 uniform float offset = 0.0;
-uniform float brightness = 1.2;
-
-vec4 PostFX(sampler2D tex, vec2 uv)
-{
-	float renderWidth = u_frameSize.x;
-	float renderHeight = u_frameSize.y;
-
-    vec4 c = vec4(0.0);
-    float size = stitchingSize;
-    vec2 cPos = uv*vec2(renderWidth, renderHeight);
-    vec2 tlPos = floor(cPos/vec2(size, size));
-    tlPos *= size;
-
-    int remX = int(mod(cPos.x, size));
-    int remY = int(mod(cPos.y, size));
-
-    if (remX == 0 && remY == 0) tlPos = cPos;
-
-    vec2 blPos = tlPos;
-    blPos.y += (size - 1.0);
-
-    if ((remX == remY) || (((int(cPos.x) - int(blPos.x)) == (int(blPos.y) - int(cPos.y)))))
-    {
-        if (invert == 1) c = vec4(0.2, 0.15, 0.05, 1.0);
-        else c = texture(tex, tlPos*vec2(1.0/renderWidth, 1.0/renderHeight))*1.4;
-    }
-    else
-    {
-        if (invert == 1) c = texture(tex, tlPos*vec2(1.0/renderWidth, 1.0/renderHeight))*1.4;
-        else c = vec4(0.0, 0.0, 0.0, 1.0);
-    }
-
-    return c;
-}
+uniform float brightness = 1.3;
+uniform bool shake;
 
 vec4 applyBloom(vec4 color, vec2 uv)
 {
@@ -80,10 +47,10 @@ vec4 applyBloom(vec4 color, vec2 uv)
 vec4 applyVignette(vec4 color)
 {
     vec2 position = (gl_FragCoord.xy / u_frameSize) - vec2(0.5);           
-    float dist = length(position);
+    float dist = length(position * vec2(u_frameSize.x/u_frameSize.y, 1.0));
 
-    float radius = 1.4;
-    float softness = 1;
+    float radius = 1.3;
+    float softness = .8;
     float vignette = smoothstep(radius, radius - softness, dist);
 
     color.rgb = color.rgb - (1.0 - vignette);
@@ -93,12 +60,12 @@ vec4 applyVignette(vec4 color)
 
 vec4 applyScanline(vec4 color, vec2 uv)
 {
-    float frequency = u_frameSize.y/3.0;
+    float frequency = u_frameSize.y/10.0;
     // Scanlines method 2
     float globalPos = (uv.y + offset) * frequency;
-    float wavePos = cos((fract(globalPos) - 0.5)*1.5);
+    float wavePos = cos((fract(globalPos) - 0.5)* 1.1);
 
-    return mix(vec4(0.2, 0.0, 0.5, 1), color, wavePos);
+    return mix(vec4(0.1, 0.0, 0.4, 1), color, wavePos);
 
 }
 
@@ -114,10 +81,22 @@ vec4 applyPosterization(vec4 color)
     return vec4(result, 1.0);
 }
 
+vec2 curve(vec2 uv)
+{
+	uv = (uv - 0.5) * 2.0;
+	uv *= 1.1;
+	uv.x *= 1.0 + pow((abs(uv.y) / 5.0), 2.0);
+	uv.y *= 1.0 + pow((abs(uv.x) / 4.0), 2.0);
+	uv = (uv / 2.0) + 0.5;
+	uv =  uv *0.92 + 0.04;
+
+	return uv;
+}
+
 vec2 fisheyeUV()
 {
-    const float PI = 2;
-    float aperture = 95.0;
+    const float PI = 3.1415926535897932384626433832795;
+    float aperture = 180.0;
     float apertureHalf = 0.5*aperture*(PI/180.0);
     float maxFactor = sin(apertureHalf);
 
@@ -163,13 +142,37 @@ vec4 applyPixelizer(vec2 uv)
     return vec4(tc, 1.0);
 }
 
+vec4 applyBlur(vec4 color, vec2 uv)
+{
+    float offset[3] = float[](0.0, 1.3846153846, 3.2307692308);
+    float weight[3] = float[](0.2, 0.2, 0.2);
+
+        // Texel color fetching from texture sampler
+    vec3 texelColor = color.rgb*weight[0];
+
+    for (int i = 1; i < 3; i++)
+    {
+        texelColor += texture(texture0, uv + vec2(offset[i])/u_frameSize.x, 0.0).rgb*weight[i];
+        texelColor += texture(texture0, uv - vec2(offset[i])/u_frameSize.y, 0.0).rgb*weight[i];
+    }
+
+    return vec4(texelColor, 1.0);
+}
+
 void main()
 {
     // vec2 uv = fragTexCoord;
-    vec2 uv = fisheyeUV();
+    float dim = min(u_frameSize.x, u_frameSize.y);
+
+
+    vec2 uv = curve(fragTexCoord);
+    vec4 color = texture(texture0, uv);
+    if (shake)
+    {
+        color = applyBlur(color, uv);
+    }
     // vec4 vfx = PostFX(texture0, uv);
     // Texel color fetching from texture sampler
-    vec4 color = texture(texture0, uv);
     // color = vfx;
     // color = applyPixelizer(uv);
     color = applyPosterization(color);
@@ -177,8 +180,33 @@ void main()
     color = applyBloom(color, uv);
     color = applyScanline(color, uv);
     // color = mix(color, vfx, 1);
+
     
     color.rgb = pow(color.rgb, vec3(1.0/brightness));
     // NOTE: Implement here your fragment shader code
+
+    // clipped unwanted uvs only render square
+	if (uv.y < 0.0 || uv.y > 1.0)
+		color *= 0.0;
+    
+    if (uv.x < 0.0 || uv.x > 1.0)
+		color *= 0.0;
+    
+    if (u_frameSize.x > u_frameSize.y)
+    {
+        float minx = (u_frameSize.x - u_frameSize.y) / (2.0 * u_frameSize.x);
+        float maxx = (u_frameSize.x + u_frameSize.y) / (2.0 * u_frameSize.x);
+        if (uv.x < minx || uv.x > maxx)
+            color *= 0.0;
+    }
+    else
+    {
+        float miny = (u_frameSize.y - u_frameSize.x) / (2.0 * u_frameSize.y);
+        float maxy = (u_frameSize.y + u_frameSize.x) / (2.0 * u_frameSize.y);
+        if (uv.y < miny || uv.y > maxy)
+            color *= 0.0;
+    }
+
+
     finalColor = color; 
 }
