7b378876d5b43eff3fa7944ff19764eab591c481
Heartless2000 <h26lyu@gmail.com>
2026-03-04 22:25:47 -0500

    update

--- Assets/Level_Editor/TlieSet/tileSet.tsx
+++ Assets/Level_Editor/TlieSet/tileSet.tsx
@@ -1,4 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileSet" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="../../Texture/SpriteAtlas.png" width="320" height="320"/>
+<tileset version="1.10" tiledversion="1.11.2" name="tileSet" tilewidth="32" tileheight="32" tilecount="15000" columns="10">
+ <image source="../../Texture/SpriteAtlas.png" width="320" height="480"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="110" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="111" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="112" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="113" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="114" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="120" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="121" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="122" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="123" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="124" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="130" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="131" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="132" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
